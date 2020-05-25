df9008c704e60a8522a14e9eb53a686cf15a9ac4
Cathrin Whitmer <catwhitmer@gmail.com>
2020-05-25 21:17:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Your code here
+function saturdayFun (fun="roller-skate") {
+  return`This Saturday, I want to ${fun}!`
+}
+
+function mondayWork(activity="go to the office") {
+  return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective (string="*") {
+  return function(param="special") {
+    return `You are ${string}${param}${string}!`
+  }
+}
+
+let calculator = {
+  
+}
